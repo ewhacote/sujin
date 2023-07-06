e499b37b69de2e272d0bc5b7a80b2d18239ff58f
mutjin08 <132420423+mutjin08@users.noreply.github.com>
2023-07-06 20:45:14 +0900

    Create 160585_혼자서 하는 틱택토.py
    23-07-06

---
+++ 프로그래머스/week2/160585_혼자서 하는 틱택토.py
@@ -0,0 +1,31 @@
+def solution(board):
+    answer = -1
+    strboard = "".join(board)
+    valid = strboard.count("0") - strboard("X")
+    if valid not in [0,1]:
+        return 0
+    
+    colboard = list(zip(*board))
+    ocnt = 0
+    xcnt = 0
+    
+    for i in range(3):
+        if colboard[i].count("O")==3 or board[i].count("O")==3:
+            ocnt+=1
+        if colboard[i].count("X")==3 or board[i].count("X")==3:
+            xcnt+=1
+            
+    for i in range(0,3,2):
+        if board[0][i]==board[1][1]==board[2][2-i]=="O":
+            ocnt+=1
+        if board[0][i]==board[1][1]==board[2][2-i]=="X":
+            xcnt+=1
+            
+    if ocnt and xcnt:
+        return 0
+    if ocnt==1 and valid==0:
+        return 0
+    if xcnt==1 and valid>=1:
+        return 0
+            
+    return 1
