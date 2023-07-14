55ce49e6af3f67c3d7b0f7aee4023eb7b12d100b
mutjin08 <132420423+mutjin08@users.noreply.github.com>
2023-07-14 23:42:46 +0900

    23-07-14

---
+++ 프로그래머스/week3/154538_숫자 변환하기.py
@@ -0,0 +1,15 @@
+def solution(x, y, n):
+    dp = [9999999]*(y+1)
+    dp[x]=0
+    for i in range(x,y+1):
+        if i+n<=y:
+            dp[i+n]=min(dp[i+n],dp[i]+1)
+        if i*2<=y:
+            dp[i*2]=min(dp[i*2],dp[i]+1)
+        if i*3<=y:
+            dp[i*3]=min(dp[i*3],dp[i]+1)
+    
+    if dp[y]==9999999:
+        return -1
+    else:
+        return dp[y]
