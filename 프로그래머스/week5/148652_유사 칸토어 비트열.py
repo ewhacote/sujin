3d86b15d7fb484ce25a2ca16b77f3403528dd9d7
mutjin08 <132420423+mutjin08@users.noreply.github.com>
2023-07-25 00:11:36 +0900

    Update 148652_유사 칸토어 비트열.py

--- 프로그래머스/week5/148652_유사 칸토어 비트열.py
+++ 프로그래머스/week5/148652_유사 칸토어 비트열.py
@@ -1,24 +1,12 @@
 def solution(n, l, r):
-    q_l = quin(l - 1)
-    q_r = quin(r)
-    count_l = decode(q_l)
-    count_r = decode(q_r)
-    return count_r - count_l
+    answer = r-l+1
+    for num in range(l-1,r):
+        while num>=1:
+            a,b=divmod(num,5)
+            if b==2 or a==2:
+                answer-=1
+                break
+            num=a
 
-def quin(n) :
-    result = []
-    while n > 0 :
-        n,mod = divmod(n,5)
-        result.append(mod)
-    return result[::-1]
 
-def decode(quinary) :
-    result = 0
-    length = len(quinary)
-    dec = [0, 1, 2, 2, 3]
-    for i, x in enumerate(quinary) :
-        exponent = length-i-1
-        result += (4**exponent)*dec[x]
-        if x == 2 :
-            break    
-    return result
+    return answer
