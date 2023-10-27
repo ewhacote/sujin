228713ceeaee490beff020b653c738b0c1e92fee
mutjin08 <132420423+mutjin08@users.noreply.github.com>
2023-10-27 23:21:25 +0900

    Update 1027.py

--- 1027.py
+++ 1027.py
@@ -1,1 +1,24 @@
 
+import sys
+input = sys.stdin.readline
+
+def solution(priorities, location):
+    answer = 0
+    queue =  [(i, p) for i, p in enumerate(priorities)]
+
+    while True:
+        cur = queue.pop(0)
+
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+
+if __name__ == "__main__":
+
+    priorities = list(map(int, input().split()))
+    location = int(input())
+
+    print(solution(priorities, location))
